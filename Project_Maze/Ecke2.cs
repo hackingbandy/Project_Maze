740bcc8a4fa1cff510baf78e9da19053eb47f6ca
Andrey Kudryavtsev <kudryavtsev.andrey@siemens.com>
2022-12-05 12:08:27 +0100

    maze wird erfolgreich übergeben

--- Project_Maze/Ecke2.cs
+++ Project_Maze/Ecke2.cs
@@ -10,8 +10,9 @@ namespace Project_Maze
     {
         //IDEE: Liste von 1-19 sackgasse = 0, Exit = 1, Start = Start.
         public int Ecke {get; set;}
-        public string? Ort { get; set;}
-        public bool besucht { get; set;}
+        public bool IsStart { get; set;}
+        public string? Location { get; set;}
+        public bool IsVisited { get; set;}
         public bool IsExit { get; set;}
     }
 }
--- Project_Maze/Program.cs
+++ Project_Maze/Program.cs
@@ -1,37 +1,50 @@
 ﻿using System;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Project_Maze // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {           
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello");
             ///
             /// Maze 
-            ///  1 --> 3 --> 6 
+            ///  1 --> 3 --> 5 
             ///  |     |
             ///  2     4
-            ///        |
-            ///        5
-            
+            ///        
+            ///  
+           List<Ecke2> ls = new List<Ecke2>();
+           Program p = new Program();
+            EventArgs e = new EventArgs();
+
+            p.Load_Maze(p, e);            
+
         }
         public void Load_Maze(object sender, EventArgs e)
         {
             List<Ecke2> maze = new List<Ecke2>();
             Fill(ref maze);
+            suche(ref maze);
+            
         }
         void Fill(ref List<Ecke2> mazeList)
         {
-            mazeList.Add(new Ecke2 { Ecke = 1, Ort = "Ort_1" });
-            mazeList.Add(new Ecke2 { Ecke = 2, Ort = "Ort_2", IsExit = false });
-            mazeList.Add(new Ecke2 { Ecke = 3, Ort = "Ort_3" });
-            mazeList.Add(new Ecke2 { Ecke = 4, Ort = "Ort_4", IsExit = false });
-            mazeList.Add(new Ecke2 { Ecke = 5, Ort = "Ort_5", IsExit = true });
+            mazeList.Add(new Ecke2 { Ecke = 1, Location = "Ort_1", IsStart = true });
+            mazeList.Add(new Ecke2 { Ecke = 2, Location = "Ort_2", IsExit = false });
+            mazeList.Add(new Ecke2 { Ecke = 3, Location = "Ort_3" });
+            mazeList.Add(new Ecke2 { Ecke = 4, Location = "Ort_4", IsExit = false });
+            mazeList.Add(new Ecke2 { Ecke = 5, Location = "Ort_5", IsExit = true });
         }
-        void suche(int suche, ref List<Ecke2> maze)
+        void suche(ref List<Ecke2> maze)
         {
-            Console.WriteLine(maze);
+            Console.WriteLine(maze[0].Location);
         }
+        
+        
+
+
     }
 }
