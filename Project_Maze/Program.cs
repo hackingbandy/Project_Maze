ce828a00af52a3424640f4ef369ed86a27eb4739
Andrey Kudryavtsev <kudryavtsev.andrey@siemens.com>
2022-12-22 16:56:42 +0100

    Code optimert Ausgabe Lösungsweg

--- Project_Maze/Program.cs
+++ Project_Maze/Program.cs
@@ -7,38 +7,36 @@ namespace Project_Maze
     {
         static void Main(string[]args)
         {
-            Program p = new Program();
-            EventArgs e = new EventArgs();
-
-            p.load_maze(p, e);
+            Program p = new Program();            
+            p.load_maze();
         }
        
-        public void load_maze(object sender, EventArgs e){
+        public void load_maze(){
             List<Ecke> irrg = new List<Ecke>();
             fill_maze(ref irrg);
             search_maze(ref irrg);
         }
 
         void fill_maze(ref List<Ecke> irrg){
-            irrg.Add(new Ecke{ Value = 18, Location = "Ort_1", IsStart = true});
+            irrg.Add(new Ecke{ Value = 18, Location = "L", IsStart = true});
             irrg.Add(new Ecke{ Value = 19, Location = "Ort_2"});
-            irrg.Add(new Ecke{ Value = 16, Location = "Ort_3"});
+            irrg.Add(new Ecke{ Value = 16, Location = "Ö"});
             irrg.Add(new Ecke{ Value = 17, Location = "Ort_4"});
-            irrg.Add(new Ecke{ Value = 14, Location = "Ort_5"});
+            irrg.Add(new Ecke{ Value = 14, Location = "S"});
             irrg.Add(new Ecke{ Value = 15, Location = "Ort_6"});
-            irrg.Add(new Ecke{ Value = 12, Location = "Ort_7"});
+            irrg.Add(new Ecke{ Value = 12, Location = "U"});
             irrg.Add(new Ecke{ Value = 13, Location = "Ort_8"});
-            irrg.Add(new Ecke{ Value = 10, Location = "Ort_9"});
+            irrg.Add(new Ecke{ Value = 10, Location = "N"});
             irrg.Add(new Ecke{ Value = 11, Location = "Ort_10"});
-            irrg.Add(new Ecke{ Value = 8, Location = "Ort_11"});
+            irrg.Add(new Ecke{ Value = 8, Location = "G"});
             irrg.Add(new Ecke{ Value = 9, Location = "Ort_12"});
-            irrg.Add(new Ecke{ Value = 6, Location = "Ort_13"});
+            irrg.Add(new Ecke{ Value = 6, Location = "S"});
             irrg.Add(new Ecke{ Value = 7, Location = "Ort_14"});
-            irrg.Add(new Ecke{ Value = 4, Location = "Ort_15"});
+            irrg.Add(new Ecke{ Value = 4, Location = "W"});
             irrg.Add(new Ecke{ Value = 5, Location = "Ort_16"});
-            irrg.Add(new Ecke{ Value = 2, Location = "Ort_17"});
+            irrg.Add(new Ecke{ Value = 2, Location = "E"});
             irrg.Add(new Ecke{ Value = 3, Location = "Ort_18"});
-            irrg.Add(new Ecke{ Value = 1, Location = "Ort_19", IsExit = true});
+            irrg.Add(new Ecke{ Value = 1, Location = "G", IsExit = true});
         }
        
         void search_maze(ref List<Ecke> irrg){
@@ -61,11 +59,14 @@ namespace Project_Maze
                 }                                         
                 if (ecke.Left == true || ecke.IsStart || ecke.IsExit)
                 {
-                    Console.WriteLine(ecke.Location);
+                    if(ecke.IsExit== true)
+                    Console.WriteLine(ecke.Location);                  
+                    else
+                    Console.Write($"{ecke.Location} -> ");
                 }
                 if (ecke.IsExit == true)
                 {
-                    Console.WriteLine($"Endpunkt: {ecke.Location}");
+                    Console.WriteLine($"\nEndpunkt: {ecke.Location}");
                 }
                 tmpint = ecke.Value;
             }
